52f7fcc655c7ee1544183c02dc47e6286c094c89
Kavishka Sinhabahu <167524790+kavishka0917@users.noreply.github.com>
2024-05-01 10:28:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import StudentRegistrationMobile from "./screens/StudentRegistrationMobile";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+    "Inter-ExtraBold": require("./assets/fonts/Inter-ExtraBold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="StudentRegistrationMobile"
+              component={StudentRegistrationMobile}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;
---
+++ GlobalStyles.js
@@ -0,0 +1,19 @@
+/* fonts */
+export const FontFamily = {
+  interExtraBold: "Inter-ExtraBold",
+  interBold: "Inter-Bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_smi: 13,
+};
+/* Colors */
+export const Color = {
+  colorBlack: "#000",
+  colorLinen: "#f4e8e1",
+  colorDarkslategray: "#444",
+};
+/* border radiuses */
+export const Border = {
+  br_mini: 15,
+};
